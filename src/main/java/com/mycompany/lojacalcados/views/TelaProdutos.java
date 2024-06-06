31f60b43450d2460882277f926f71020ed7e6bd8
Rafael Martins dos Santos <151884498+RafaelMartinns@users.noreply.github.com>
2024-06-05 21:40:47 -0300

    Update TelaProdutos

--- src/main/java/com/mycompany/lojacalcados/views/TelaProdutos.java
+++ src/main/java/com/mycompany/lojacalcados/views/TelaProdutos.java
@@ -21,10 +21,7 @@ import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author felip
- */
+
 public class TelaProdutos extends javax.swing.JFrame {
 
     /**
